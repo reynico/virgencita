b1150ca3e9a446c5e78934daeb91fdb27e9a6c25
Nico Rey <nrey@elementum.com>
2019-02-01 12:08:03 -0300

    Fixed lat lon variables

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ def show_index():
     client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     client_response = reader.city(client_ip)
     apiKey = os.environ['FORECAST_API_KEY']
-    fio = ForecastIO.ForecastIO(apiKey, latitude=client_response.location.latitude, longitude=client_response.location.latitude)
+    lat = str(client_response.location.latitude)
+    lon = str(client_response.location.longitude)
+    fio = ForecastIO.ForecastIO(apiKey, latitude=lat, longitude=lon)
     current = FIOCurrently.FIOCurrently(fio)
     return render_template("index.html", probabilidad = int(current.precipProbability*100))
 
