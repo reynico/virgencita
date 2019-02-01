40fc17b4509b2c26e4044dd36b597a31e5251674
Gabriel Nasr <gnasr@elementum.com>
2019-02-01 11:24:51 -0300

    Move apiKey to env var

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from forecastiopy import *
 from flask import Flask, render_template, request, url_for
+import os
 app = Flask(__name__)
 Buenos_Aires = [-34.6037, -58.3816]
 
 @app.route('/')
 def show_index():
-    fio = ForecastIO.ForecastIO('xxx', latitude=Buenos_Aires[0], longitude=Buenos_Aires[1])
+    apiKey = os.environ['FORECAST_API_KEY']
+    fio = ForecastIO.ForecastIO(apiKey, latitude=Buenos_Aires[0], longitude=Buenos_Aires[1])
     current = FIOCurrently.FIOCurrently(fio)
     return render_template("index.html", probabilidad = int(current.precipProbability*100))
 
