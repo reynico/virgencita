ae7ee85ccab9e7908df6ea78e606594d435c5d3e
Nico Rey <rey.nico@gmail.com>
2019-02-02 12:44:07 -0300

    New features

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
 from flask import Flask, render_template, request, url_for
-from forecastiopy import *
+import sys
+import requests
+import requests_cache
 import geoip2.database
 import random
 import os
 
+requests_cache.install_cache('virgencita', expire_after=1800)
 
-app = Flask(__name__)
-reader = geoip2.database.Reader('geo.mmdb')
-
-@app.route('/')
-def show_index():
-    client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    apiKey = random.sample(set([os.environ['FORECAST_API_KEY_1'], os.environ['FORECAST_API_KEY_2'], os.environ['FORECAST_API_KEY_3']]), 1)
+def getLocation(client_ip):
+    reader = geoip2.database.Reader('geo.mmdb')
     try:
-    	client_response = reader.city(client_ip)
-    	lat = str(client_response.location.latitude)
-    	lon = str(client_response.location.longitude)
+        client_response = reader.city(client_ip)
+        lat = str(round(client_response.location.latitude,2))
+        lon = str(round(client_response.location.longitude,2))
     except:
         lat = '-34.6037'
         lon = '-58.3816'
         pass
-    fio = ForecastIO.ForecastIO(apiKey[0], latitude=lat, longitude=lon)
-    current = FIOCurrently.FIOCurrently(fio)
-    return render_template("index.html", probabilidad = int(current.humidity*100))
+    print('Lat/Lon: ' + lat + ', ' + lon, file=sys.stdout)
+    sys.stdout.flush()
+    return(lat,lon)
+
+def getApiKey():
+    return(random.sample(set([
+        os.environ['FORECAST_API_KEY_1'], 
+        os.environ['FORECAST_API_KEY_2'], 
+        os.environ['FORECAST_API_KEY_3']]), 
+    1))
+
+def getHumidity(client_ip):
+    coords = getLocation(client_ip)
+    r = requests.get('https://api.darksky.net/forecast/%s/%s,%s' % 
+        (getApiKey()[0], coords[0],coords[1]))
+    print('Is cached: ' + str(r.from_cache), file=sys.stdout)
+    sys.stdout.flush()
+    data = r.json()
+    return(int(data['currently']['humidity']*100))
+
+app = Flask(__name__)
+@app.route('/')
+def show_index():
+    client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    print('Client IP: ' + str(client_ip), file=sys.stdout)
+    sys.stdout.flush()
+    return render_template("index.html", probabilidad = getHumidity(client_ip))
 
 if __name__ == '__main__':
     app.run()
