ac8304f17b9a7b0cabcd204193a1e8fc7f6e754e
Nico Rey <rey.nico@gmail.com>
2019-02-02 18:04:42 -0300

    Disable flask logging, pep8, variable name

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ import requests_cache
 import geoip2.database
 import random
 import os
+import logging
 
 host = os.environ.get('APP_HOST', '127.0.0.1')
 port = os.environ.get('APP_PORT', 5000)
 
 requests_cache.install_cache('virgencita', expire_after=1800)
+
 
 def getLocation(client_ip):
     reader = geoip2.database.Reader('geo.mmdb')
@@ -38,20 +40,25 @@ def getHumidity(client_ip):
     coords = getLocation(client_ip)
     r = requests.get('https://api.darksky.net/forecast/%s/%s,%s' %
                      (getApiKey(), coords[0], coords[1]))
-    print('Is cached: ' + str(r.from_cache), file=sys.stdout)
+    print('Is cached: ' + str(r.from_cache) + '\n', file=sys.stdout)
     sys.stdout.flush()
     data = r.json()
-    return((int((data['currently']['humidity']*100))+int(data['currently']['precipProbability']*100))/2)
+    humidity = int(data['currently']['humidity']*100)
+    precip = int(data['currently']['precipProbability']*100)
+    return((humidity + precip)/2)
 
 app = Flask(__name__)
 
-
+app.logger.disabled = True
+log = logging.getLogger('werkzeug')
+log.disabled = True
 @app.route('/')
 def show_index():
     client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     print('Client IP: ' + str(client_ip), file=sys.stdout)
     sys.stdout.flush()
-    return render_template("index.html", analytics_id=getAnalyticsKey(), probabilidad=getHumidity(client_ip))
+    return render_template("index.html", analytics_id=getAnalyticsKey(),
+                           probability=getHumidity(client_ip))
 
 if __name__ == '__main__':
     app.run(host, port)
